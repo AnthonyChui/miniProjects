e59b573039096d5e27b41d56b46273e4c0800fb8
Anthony Chui <154588467+AnthonyChui@users.noreply.github.com>
2024-07-19 21:51:45 -0600

    Change click to hover

--- expandingCards/script.js
+++ expandingCards/script.js
@@ -1,9 +1,12 @@
 const cards = document.querySelectorAll('.card');
 cards.forEach((card) => {
-    card.addEventListener("click", () => {
+    card.addEventListener("mouseover", () => {
         cards.forEach((card) =>{
             card.classList.remove("active");
         })
         card.classList.add("active");
+    });
+    card.addEventListener("mouseout", () => {
+            card.classList.remove("active");
     });
 });
